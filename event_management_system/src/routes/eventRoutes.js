6e578f21640ee5b7dc3f3b2fec4deb273bbd7e17
sujashab0512 <sujasha.bhattacharya@cognizant.com>
2025-11-29 18:12:21 +0530

    Update eventRoutes.js

--- event_management_system/src/routes/eventRoutes.js
+++ event_management_system/src/routes/eventRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { createEvent } = require('../controllers/eventController');
+const { createEvent,uploadImage } = require('../controllers/eventController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 // Allow Organizer and Attendee to create events
 router.post('/', authMiddleware(['Organizer', 'Attendee']), createEvent);
-router.post('/:id/image', authMiddleware(['Organizer']), upload.single('image'), uploadImage);
+router.post('/:id/image', authMiddleware(['Organizer']), uploadImage);
 module.exports = router;
